93d6529a3e28c6b48d1cbf118db4406a827a0114
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-06 12:20:09 -0700

    Fixing errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longInt));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongInt));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-	
 	return (0);
 }
