f86af75d3c3226bb9da24964bfeee7396edb88b6
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-06 08:06:42 -0700

    HotFix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: 0 Always (Succes)
@@ -9,6 +8,6 @@
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-		return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+		return (0);
 }
