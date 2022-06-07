e328a20ff6f8d670a985c7e815f4fb24534d7528
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-06 21:23:09 -0700

    Rushing

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entr point
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+
+		if (n == 9)
+		{
+			putchar(n + '0');
+		}
+		else
+		{
+			putchar(n + '0');
+			putchar(',');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
