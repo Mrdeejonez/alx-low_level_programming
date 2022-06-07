6f3dd8d9150785eb429d65ecd49d23bd48989a5e
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-07 14:56:06 -0700

    House keeping

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,3 @@
-
 #include <unistd.h>
-
-int _putchar(char c);
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
-}
+#include "_putchar.c"
+int _putchar(char a);
