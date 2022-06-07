dafa647bada3af928b18c0e4e287ebd91250c53b
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-06 21:02:16 -0700

    Rushing

---
+++ 0x01-variables_if_else_while/7-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entr point
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char singleLetter;
+	int num;
+
+	for (num = 0 ; num <= 10 ; num++ )
+	{
+		putchar(num%10 + '0');
+	}
+	for (singleLetter = 'a' ; singleLetter <= 'f'; singleLetter++)
+	{
+		putchar(singleLetter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
