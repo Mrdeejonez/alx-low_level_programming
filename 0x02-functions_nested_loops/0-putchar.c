33ee556885e42af4cdcf1623dc36e4ff40ec6428
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-07 15:30:46 -0700

    Trying a different approach

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,6 @@
 
 int _putchar(char c);
 /**
- *
  * main - Entry point
  *
  * Return: 0 Always (Success)
@@ -11,15 +10,15 @@ int _putchar(char c);
 
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-        return (0);
+	return (0);
 }
