fcdce74ae325d0dbb2029885e213adbafe72dd00
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-06 18:03:04 -0700

    Printing the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entr point
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char singleLetter;
+
+	for (singleLetter = 'a' ; singleLetter <= 'z'; singleLetter++)
+	{
+		putchar(singleLetter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,5 +2,5 @@
 
 1-last_last_digit tells you what the lastt diit is and whether it is greater than 5, less than 6 and whether it is 0
 
-
+2-print_alphabet prints the alphabet
 
