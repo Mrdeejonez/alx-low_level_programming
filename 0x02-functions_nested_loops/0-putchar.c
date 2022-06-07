048d616633bd7bce5ece1c102dab7b4e9d242009
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-07 14:36:33 -0700

    Swapping main and putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,17 @@
-#include <stdio.h>
-#include "main.h"
+#include <unistd.h>
 
 int _putchar(char c);
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * main - Entry point
- *
- * Return: 0 Always (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+        return (write(1, &c, 1));
 }
+
