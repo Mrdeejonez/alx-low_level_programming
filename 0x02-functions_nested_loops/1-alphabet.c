419ea2fdcdefb5685a95b272266c88be3d1ade45
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-07 18:18:59 -0700

    removing links

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,21 +6,6 @@
  *
  * Return: 0 Always (Success)
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-void print_alphabet(void)
-{
-	char alpha;
-
-	for (alpha = 'a' ; alpha < 'z' ; alpha++)
-	{
-		_putchar(alpha);
-	}
-	_putchar('\n');
-}
 
 int main(void)
 {
