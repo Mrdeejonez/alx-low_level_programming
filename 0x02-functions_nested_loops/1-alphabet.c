c458f4c74f02baa8c90a32c55dbfdc3b5035b4c3
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-07 18:08:18 -0700

    removing links

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,6 @@
 #include <unistd.h>
 #include "main.h"
 
-
-
-
 /**
  * main - Entry point
  *
@@ -17,29 +14,3 @@ int main(void)
 	return (0);
 }
 
-void print_alphabet(void)
-{
-	char alpha;
-
-	for (alpha = 'a' ; alpha < 'z' ; alpha++)
-	{
-		_putchar(alpha);
-	}
-	_putchar('\n');
-}
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-void print_alphabet(void)
-{
-	char alpha;
-
-	for (alpha = 'a' ; alpha < 'z' ; alpha++)
-	{
-		_putchar(alpha);
-	}
-	_putchar('\n');
-}
