d01c349088baed833460855780db591d1c886e81
Dingaan Thatho <dingaanthatho@gmail.com>
2022-06-08 09:16:19 -0700

    checking if  is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * checks of the character is lower
+ *
+ * Retruns: 1 (Success) 
+ * Returns: 0 (Otherwise)
+ */
+
+int _islower(int c)
+{
+	char checker = c;
+	int val = 0;
+
+	if(checker >= 'a' && checker <= 'z')
+	{
+		val = 1;
+	}	
+	
+	return (val);
+}
